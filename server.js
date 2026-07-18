8549e9b7483262331a1ad711cd654a69a3bbe77c
serajhaidar <serajryn@gmail.com>
2026-07-18 13:22:45 +0545

    Week 4: minor backend updates

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./src/config/db');
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const taskRoutes = require('./src/routes/taskRoutes');
 const movieRoutes = require('./src/routes/movieRoutes');
+
 
 app.get('/', (req, res) => {
     res.send('Express Server is running');
